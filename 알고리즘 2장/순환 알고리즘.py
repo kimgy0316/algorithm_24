f00757999025b75504562ecaca29337c91b5dcc0
Kim Gyeong Yeon <kimtree1230@naver.com>
2024-04-02 18:29:12 +0900

    aaa

--- 알고리즘 2장/순환 알고리즘.py
+++ 알고리즘 2장/순환 알고리즘.py
@@ -6,5 +6,5 @@ def hanoi_tower(n, fr, tmp, to):
         print("원판 %s: %s --> %s" % (n, fr, to))
         hanoi_tower(n-1, tmp, fr, to)
 
-n = int(input("원판의 갯수를 입력하시오(타워는 3개) : "))
+n = int(input("원판의 갯수(타워는 3개) >>> "))
 hanoi_tower(n, 'A', 'B', 'C')
